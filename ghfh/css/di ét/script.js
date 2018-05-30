d55fd63cbe924c418f355474a8da781898e4b9ea
kkingkangg <38235739+kkingkangg@users.noreply.github.com>
2018-05-30 15:06:09 +0700

    bài tập cut web
    dâsd

--- ghfh/css/di ét/script.js
+++ ghfh/css/di ét/script.js
@@ -2,14 +2,70 @@
 alert('bạn đã bị hack')
 
 
-//bai1
+//bài 1
 function bai1(x) {
-	alert(x*x)
+	alert(x*x);
+	return (x**2);
 }
 
 
 
-//bai2
+//bài 2
 function bai2(a,b,c) {
-	alert((3*a+2*b-c)*(3*a+2*b-c))
+	let x=3*a+2*b-c;
+	return bai1(x);
 }
+
+
+//bài 3
+function bai3(str) {
+  return str.length > 10 ? str.substring(0, 10) + '...' : str;
+}
+
+
+
+
+
+
+//bài 4
+function bai4(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+
+
+//bài 5
+function bai5(a,b,c,d) {
+	return Math.min(a,b,c,d);
+}
+
+
+
+//bài 6
+function bai6() {
+
+}
+
+
+
+//bài 7
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.sort();
+    fruits.reverse();
+    document.getElementById("demo").innerHTML = fruits;
+}
+
+
+
+
+
+
+
+
+
+
+
